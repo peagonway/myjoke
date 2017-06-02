516d06aabe570630aa4965deb4e6dd94fa2205a8
worldpeace <421268460@qq.com>
2017-06-02 18:07:31 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,5 @@ export default {
 </script>
 
 <style>
-html,body,div,p,ul,ol,li{margin:0;padding:0;}
-ul,ol,li{list-style: none}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,37 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
-import MintUI from 'mint-ui'
+import mintUi from 'mint-ui'
 import 'mint-ui/lib/style.min.css'
-Vue.use(MintUI)
+Vue.use(mintUi)
 
 import store from './store'
-
 import App from './App'
 import router from './router'
 
-import '../static/css/base.css'
-import '../static/css/main.css'
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   store,
+  router,
   template: '<App/>',
   components: { App }
 })
-
-
-
-let html = document.documentElement;
-function setRem(){
-	window.rem = html.getBoundingClientRect().width / 320 ;
-	console.log(rem)
-	html.style.fontSize = window.rem + 'px';
-}
-setRem()
-
-window.addEventListener("resize", setRem, false)
