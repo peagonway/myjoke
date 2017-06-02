ceaeabd5ddc0cb7cef00ffed9172fcb496ce07e6
worldpeace <421268460@qq.com>
2017-06-02 18:08:03 +0800

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,62 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
 Vue.prototype.$ajax = axios
-
 Vue.use(Vuex)
 
-const API = {
-	userlogin: 'http://www.369college.com/369education/yzh/inter/login' 
+const jokeAPI = {
+  'text':'http://route.showapi.com/341-1'
+}
+const params = {
+  'showapi_appid': '38662',
+  'showapi_sign': 'aa2ab98ad089496bb50999d41a58d7ea'
 }
 
 const store = new Vuex.Store({
-	state: {
-		username: ""
+	state:{
+		pageTab: 1,
+		pageTit: "笑话",
+    textList: []
 	},
-	computed:{
+	computed: {
+		pageTit (state) {
+			switch (state.pageTab){
+				case 1 : return "笑话"; return false;
+				case 2 : return "图片"; return false;
+				case 3 : return "笑话"; return false;
+			}
+		}
 	},
-	mutations: {
+	mutations:{
+		changePage (state,title) {
+			state.pageTit  = title;
+		},
+    getJoketext (state,textList) {
+      state.textList = state.textList.concat(textList)
+    }
 	},
-	actions: {
-	}
+	actions:{
+		changeTit (context,title) {
+			console.log(title)
+			context.commit("changePage",title)
+		},
+    getText (context,page) {
+      let config = {
+        params,
+        page
+      }
+      axios.get(jokeAPI.text,config)
+        .then(function(rst){
+          ///console.log(rst)
+          let rstList = rst.data.showapi_res_body.contentlist
+          context.commit("getJoketext",rstList)
+        })
+    }
 
+	}
 })
 
 export default store
